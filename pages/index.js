5e111a1d131e21e7f24cf6bd8df484a7a51c06be
AndiWulanWahyuni <wulanandi06@gmail.com>
2025-05-13 23:33:37 +0800

    Fix: ubah file index.js

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import axios from "axios";
 import QRCodeComponent from "../components/QRCodeComponent";
-import "../styles/style";
+import "../styles/style.css";
 console.log("QRCodeComponent is:", QRCodeComponent);
 
 export default function Home() {
